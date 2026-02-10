93b30a10169333e0eb056b7fc30f01f1d401afd4
Caleb Eghan <caleb@lorendix.com>
2026-02-10 21:22:34 +0000

    fic help error

--- src/observix_cli/__main__.py
+++ src/observix_cli/__main__.py
@@ -1,4 +1,6 @@
-from .main import app
+from __future__ import annotations
+
+from observix_cli.cli import app
 
 if __name__ == "__main__":
     app()
--- src/observix_indexer/__main__.py
+++ src/observix_indexer/__main__.py
@@ -1,4 +1,6 @@
-from .cli import app
+from __future__ import annotations
+
+from observix_indexer.cli import app
 
 if __name__ == "__main__":
     app()
